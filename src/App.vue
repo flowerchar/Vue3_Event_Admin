7af9b16ef36cf2b45b96517cbf87bbae0f3a7a76
flowerchar <flowerchar2001@gmail.com>
2023-08-11 19:48:00 +0800

    error: 错误演示husky

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/abgiout">About</RouterLink>
       </nav>
     </div>
   </header>
