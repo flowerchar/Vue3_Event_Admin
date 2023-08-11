36a0de16ec684a444af5c0b53bc8bef1db06ffda
flowerchar <flowerchar2001@gmail.com>
2023-08-11 19:34:29 +0800

    fix: eslint配合prettier完成代码格式配置

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <img alt="Vue logo" class="logo" height="125" src="@/assets/logo.svg" width="125" />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
