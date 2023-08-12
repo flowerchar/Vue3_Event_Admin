5b2f4dd83e63f82ebf08a0e4de2a68d20a342ce8
flowerchar <flowerchar2001@gmail.com>
2023-08-12 15:29:14 +0800

    feat: 数据交互-请求工具设计

---
+++ src/utils/request.js
@@ -0,0 +1,47 @@
+import axios from 'axios'
+import { useUserStore } from '@/stores'
+import { ElMessage } from 'element-plus'
+import router from '@/router'
+
+const baseURL = 'http://big-event-vue-api-t.itheima.net'
+
+const instance = axios.create({
+  // TODO 1. 基础地址，超时时间
+  baseURL,
+  timeout: 10000
+})
+
+instance.interceptors.request.use(
+  (config) => {
+    // TODO 2. 携带token
+    const userStore = useUserStore()
+    if (userStore.token) {
+      config.headers.Authorization = userStore.token
+    }
+    return config
+  },
+  (err) => Promise.reject(err)
+)
+
+instance.interceptors.response.use(
+  (res) => {
+    // TODO 3. 处理业务失败
+    if (res.data.code === 0) {
+      return res
+    }
+    // TODO 4. 摘取核心响应数据
+    ElMessage.error(res.data.message || '服务异常')
+    return Promise.reject(res.data)
+  },
+  (err) => {
+    // TODO 5. 处理401错误
+    if (err.response?.status === 401) {
+      router.push('/login')
+    }
+    ElMessage.error(err.response.data.message || '服务异常')
+    return Promise.reject(err)
+  }
+)
+
+export default instance
+export { baseURL }
