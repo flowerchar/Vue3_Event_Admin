04f392a173bb7978608993c80bbed0a8e0399356
flowerchar <flowerchar2001@gmail.com>
2023-08-14 20:10:54 +0800

    feat: 文章分类渲染

---
+++ src/api/article.js
@@ -0,0 +1,3 @@
+import request from '@/utils/request'
+
+export const artGetChannelService = () => request.get('/my/cate/list')
--- src/views/article/ArticleChannel.vue
+++ src/views/article/ArticleChannel.vue
@@ -1,11 +1,55 @@
+<script setup>
+import PageContainer from '@/components/PageContainer.vue'
+import { artGetChannelService } from '@/api/article'
+import { ref } from 'vue'
+import { Delete, Edit } from '@element-plus/icons-vue'
+
+const loading = ref(false)
+const channelList = ref([])
+const getChannelList = async () => {
+  loading.value = true
+  const res = await artGetChannelService()
+  channelList.value = res.data.data
+  console.log(channelList.value)
+  loading.value = false
+}
+const onEditChannel = (row, $index) => {
+  console.log(row, $index)
+}
+const onDeleteChannel = (row, $index) => {
+  console.log(row, $index)
+}
+getChannelList()
+</script>
+
 <template>
   <page-container title="文章分类">
     <template #extra>
       <el-button>添加分类</el-button>
     </template>
-    按时大大
+    <el-table v-loading="loading" :data="channelList" style="width: 100%">
+      <el-table-column label="序号" type="index" width="100"></el-table-column>
+      <el-table-column label="分类名称" prop="cate_name"></el-table-column>
+      <el-table-column label="分类别名" prop="cate_alias"></el-table-column>
+      <el-table-column label="操作" width="150">
+        <template #default="{ row, $index }">
+          <el-button
+            :icon="Edit"
+            circle
+            type="primary"
+            @click="onEditChannel(row, $index)"
+          ></el-button>
+          <el-button
+            :icon="Delete"
+            circle
+            type="danger"
+            @click="onDeleteChannel(row, $index)"
+          ></el-button>
+        </template>
+      </el-table-column>
+      <template #empty>
+        <el-empty description="没有数据"></el-empty>
+      </template>
+    </el-table>
   </page-container>
 </template>
-<script setup>
-import PageContainer from '@/components/PageContainer.vue'
-</script>
