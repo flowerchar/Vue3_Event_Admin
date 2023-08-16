a4f9e4fadc4c67de53c0d1ebb62d060d1b2b6888
flowerchar <flowerchar2001@gmail.com>
2023-08-16 19:26:12 +0800

    feat: 文章新增-完善抽屉表单结构

--- src/views/article/components/ArticleEdit.vue
+++ src/views/article/components/ArticleEdit.vue
@@ -1,19 +1,60 @@
 <script lang="ts" setup>
 import { ref } from 'vue'
+import ChannelSelect from './ChannelSelect.vue'
 
 const visibleDrawer = ref(false)
 const open = (row) => {
   visibleDrawer.value = true
   console.log(row)
+  if (row.id) {
+    console.log('edit')
+  } else {
+    formModel.value = { ...defaultForm }
+    console.log('add')
+  }
 }
+const defaultForm = {
+  title: '',
+  cate_id: '',
+  cover_img: '',
+  content: '',
+  state: ''
+}
+const formModel = ref({
+  ...defaultForm
+})
 defineExpose({
   open
 })
 </script>
 
 <template>
-  <el-drawer v-model="visibleDrawer" title="TITLE">
-    <span>Hello!</span>
+  <el-drawer
+    v-model="visibleDrawer"
+    :title="formModel.id ? '编辑文章' : '添加文章'"
+    direction="rtl"
+    size="50%"
+  >
+    <!-- 发表文章表单 -->
+    <el-form ref="formRef" :model="formModel" label-width="100px">
+      <el-form-item label="文章标题" prop="title">
+        <el-input v-model="formModel.title" placeholder="请输入标题"></el-input>
+      </el-form-item>
+      <el-form-item label="文章分类" prop="cate_id">
+        <channel-select
+          v-model="formModel.cate_id"
+          width="100%"
+        ></channel-select>
+      </el-form-item>
+      <el-form-item label="文章封面" prop="cover_img"> 文件上传</el-form-item>
+      <el-form-item label="文章内容" prop="content">
+        <div class="editor">富文本编辑器</div>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary">发布</el-button>
+        <el-button type="info">草稿</el-button>
+      </el-form-item>
+    </el-form>
   </el-drawer>
 </template>
 
--- src/views/article/components/ChannelSelect.vue
+++ src/views/article/components/ChannelSelect.vue
@@ -5,6 +5,9 @@ import { artGetChannelService } from '@/api/article.js'
 defineProps({
   modelValue: {
     type: [Number, String]
+  },
+  width: {
+    type: String
   }
 })
 const emit = defineEmits(['update:modelValue'])
@@ -19,19 +22,18 @@ onMounted(() => {
 </script>
 
 <template>
-  <div>
-    <el-select
-      :modelValue="modelValue"
-      @update:modelValue="emit('update:modelValue', $event)"
-    >
-      <el-option
-        v-for="channel in channelList"
-        :key="channel.id"
-        :label="channel.cate_name"
-        :value="channel.id"
-      ></el-option>
-    </el-select>
-  </div>
+  <el-select
+    :modelValue="modelValue"
+    :style="{ width }"
+    @update:modelValue="emit('update:modelValue', $event)"
+  >
+    <el-option
+      v-for="channel in channelList"
+      :key="channel.id"
+      :label="channel.cate_name"
+      :value="channel.id"
+    ></el-option>
+  </el-select>
 </template>
 
 <style lang="scss" scoped></style>
